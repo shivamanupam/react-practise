9d5c93c3cd1d3390418d1d8849c453e2a041fcfd
Shivam Anupam <shivamanupam@Shivams-MacBook-Air.local>
2025-05-23 00:20:58 +0530

    Added Navbar component

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/navbar/Navbar";
+
 function App() {
   return (
     <>
-      <h2 className="text-3xl font-bold underline italic">Learn React</h2>
+      <Navbar />
     </>
   );
 }
